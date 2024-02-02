426c5f91a9b03cf2ad66e55079e4824f0bac2d2f
Sara Imparato <116022466+Semproniia@users.noreply.github.com>
2024-02-02 12:02:20 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# File: main.py
+
+def add_numbers(a, b):
+    result = a + b
+    print(f"The sum of {a} and {b} is: {result}")
+
+if __name__ == "__main__":
+    num1 = 5
+    num2 = 10
+    add_numbers(num1, num2)
